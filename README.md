59cf28e782bf9046eabe986bcb958aae1b86e392
duyvuxx <duy.vux88@gmail.com>
2023-04-25 21:46:07 +0700

    done_ex_1

---
+++ README.md
@@ -0,0 +1,9 @@
+# JavaScript Excercise 1
+
+## var str = 'The man who take my book.'
+
+### 1. Tim vi tri cua tu dau tien chua chu "o"
+### 2. Thay the "my book" thanh "my bag"
+### 3. Them chuoi sau vao chuoi ban dau: "I don't know that man."
+### 4. Hightlight "my bag" with 2 star like: "*my bag*"
+### 5. Tach chuoi thanh mang va tim nhung tu chua ky tu "a"
---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+var str = "The man who take my book.";
+
+// Bài 1. Tim vi tri cua tu dau tien chua chu "o"
+// let findO = str.indexOf("o")
+// let takeW = str.slice(0, findO)
+// let findW = takeW.lastIndexOf(" ") + 1
+// console.log(`Câu 1:`, findW);
+
+// // Bài 2. Thay the "my book" thanh "my bag"
+// str = str.replace("my book", "my bag");
+// console.log(`Câu 2:`, str);
+
+// // Bài 3. Them chuoi sau vao chuoi ban dau: "I don't know that man."
+// str += "I don't know that man";
+// console.log(`Câu 3:`, str);
+
+// // Bài 4. Hightlight "my bag" with 2 star like: "*my bag*"
+// str = str.replace("my bag", "*my bag*");
+// console.log(`Câu 4:`, str);
+
+// Bài 5. Tach chuoi thanh mang va tim nhung tu chua ky tu "a"
+// let arr = str.split(" ");
+// let list = [];
+
+// for (var val of arr) {
+//   if (val.includes("a")) list.push(val);
+// }
+
+// console.log(list);
