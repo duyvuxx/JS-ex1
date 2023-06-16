d457d414799b10bc29db7618d710887192c82044
duyvuxx <duy.vux88@gmail.com>
2023-06-16 13:53:02 +0700

    pratice_again

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,18 +11,33 @@ var str = "The man who take my book.";
 // console.log(`Câu 2:`, str);
 
 // // Bài 3. Them chuoi sau vao chuoi ban dau: "I don't know that man."
-// str += "I don't know that man";
+// var add = str.concat(`
+// I don't know that man.`);   /* Tự làm */
+// console.log(add);
+
+// str += "I don't know that man";    /* Tối ưu optimize */
 // console.log(`Câu 3:`, str);
 
 // // Bài 4. Hightlight "my bag" with 2 star like: "*my bag*"
-// str = str.replace("my bag", "*my bag*");
-// console.log(`Câu 4:`, str);
+// var findhighLight = replace.match(/my bag/);
+// var done = replace.replace(findhighLight[0], "*my bag*");
+// console.log(done);
 
 // Bài 5. Tach chuoi thanh mang va tim nhung tu chua ky tu "a"
+// var split = str.split(" ");
+
+// var word = "";
+// for (var i = 0; i < split.length; i++) {      /* Tự làm */
+//   if (split[i].includes("a")) {
+//     word += `${split[i]} `;
+//   }
+// }
+// console.log(word);
+
 // let arr = str.split(" ");
 // let list = [];
 
-// for (var val of arr) {
+// for (var val of arr) {                        /* Tối ưu optimize */
 //   if (val.includes("a")) list.push(val);
 // }
 
